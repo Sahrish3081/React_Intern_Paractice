0c1b321f6871bf1d00d6faeda34baf7dd8b8fba6
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-07-11 17:39:56 +0500

    React Static Page Build

---
+++ Static_Page/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import logo from './assets/images.jpg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <>
+  <div className="container">
+    <div className="head">
+      <img src={logo} alt="React Logo" />
+      <h1>React Facts</h1>
+    </div>
+
+    <div className="content">
+      <h2>Why Developers Love React</h2>
+
+      <ul>
+        <li>⚛️ React is a JavaScript library for building user interfaces.</li>
+        <li>🧩 It uses reusable components to create scalable applications.</li>
+        <li>⚡ Virtual DOM improves performance by updating only what changes.</li>
+        <li>🔄 One-way data flow makes applications predictable and easier to debug.</li>
+        <li>🌍 React is widely used in modern web development and has a strong community.</li>
+      </ul>
+    </div>
+  </div>
+</>
+    
+  )
+}
+
+export default App
